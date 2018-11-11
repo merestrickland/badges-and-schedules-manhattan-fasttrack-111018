c7c021d3cbf0fb6bc235ec3145e2124d522f87ee
Meredith Strickland <mere.strickland@gmail.com>
2018-11-11 21:07:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(arr)
+  #returns an array of badge messages
+  badge_arr = []
+  counter = 0
+  while counter < arr.length
+    each_speaker = badge_maker(arr[counter])
+    badge_arr.push(each_speaker)
+    counter += 1
+  end
+  puts badge_arr
+  #puts badge_arr.kind_of?(Array)
+end
+
+#batch_badge_creator(speakers)
+
+
+def assign_rooms(arr)
+#assign each speaker to a room 1-7
+  counter = 0
+  (1..7).each do |num|
+  puts "Hello, #{arr[counter]}! You'll be assigned to room #{num}"
+  counter += 1
+  end
+end
+
+#assign_rooms(speakers)
+
+def printer(arr)
+  puts batch_badge_creator(arr)
+  puts assign_rooms(arr)
+end
+
+#printer(speakers)
